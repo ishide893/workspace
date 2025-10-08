ba7a8a4c3d4f0aace0bed4f1804e73209ed06779
ishide <ishide@soft-co.jp>
2025-10-08 11:33:28 +0900

    最初のコミット

---
+++ helloWorld/programming/src/Exception.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exception {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int result = 0;
+        boolean success = false;
+
+        try {
+            System.out.print("整数を入力してください: ");
+            int num = Integer.parseInt(scanner.nextLine());
+            result = 100 / num;
+            success = true;
+        } catch (ArithmeticException e)  {
+            System.out.println("エラー: 整数を入力してください");
+        } finally {
+            scanner.close();
+            if (success) {
+                System.out.println("計算結果: " + result);
+            }
+            System.out.println("プログラム終了");
+        }
+    }
+}
---
+++ helloWorld/programming/src/Loop.java
@@ -0,0 +1,8 @@
+public class Loop {
+    public static void main(String[] arg){
+        int[] numbers = {2, 4, 6, 8, 10};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
